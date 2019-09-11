9304e1210fabd979123d7e39126e11c035814be2
Janine White <janine.white@gmail.com>
2019-09-11 10:31:10 -0700

    colors.py

---
+++ 102/colors.py
@@ -0,0 +1,21 @@
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+    """In the while loop ask the user to enter a color,
+       lowercase it and store it in a variable. Next check: 
+       - if 'quit' was entered for color, print 'bye' and break. 
+       - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
+       - otherwise print the color in lower case."""
+    chose_quit = False
+    while not chose_quit:
+        color = str(input("Enter a color.")).lower()
+        chose_quit = (color=="quit")
+        if chose_quit:
+            print("bye")
+            break
+        elif color in VALID_COLORS:
+            print(color)
+        else:
+            print("Not a valid color")
+    pass
