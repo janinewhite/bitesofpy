316e67cb26f7eb375948c2e887627acf7505e0f7
Janine White <janine.white@gmail.com>
2019-09-17 13:46:09 -0700

    Update summing.py

--- 1/summing.py
+++ 1/summing.py
@@ -1,8 +1,10 @@
 def sum_numbers(numbers=None):
     total = 0
-    if numbers==None or len(numbers)==0:
+    if numbers==None:
         for num in range(1,101):
             total += num
+    elif len(numbers)==0:
+        pass
     else:
         for num in numbers:
             total += int(num)
