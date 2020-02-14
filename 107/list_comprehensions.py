f57e2a15b4c2f685abfbf5647e5638578aafcc7e
Janine White <janine.white@gmail.com>
2020-02-14 08:50:55 -0700

    bites updated

--- 107/list_comprehensions.py
+++ 107/list_comprehensions.py
@@ -2,4 +2,4 @@ def filter_positive_even_numbers(numbers):
     """Receives a list of numbers, and returns a filtered list of only the
        numbers that are both positive and even (divisible by 2), try to use a
        list comprehension."""
-    pass
+   return [i for i in numbers if ((i > 0) and ((i % 2) == 0))]
